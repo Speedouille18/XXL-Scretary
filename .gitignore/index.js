60d2ca16c4d08b4d142711070ec7d01e399a574d
Speedouille18 <57258464+Speedouille18@users.noreply.github.com>
2020-08-07 14:15:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,4 +4,4 @@ const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 
-client.login("NzQwODYyMTkzODcyMTQyMzY3.XyvLrw.oqNL4ec74TwyPGcn4wMzrLMZxUw")
+client.login(process.env.TOKEN)
